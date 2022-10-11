051b239003c294176283434d1930902a27c97bce
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-11 21:56:13 +1100

    Account repo completed

---
+++ src/main/java/com/example/challenge2/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.challenge2.repository;
+
+import com.example.challenge2.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
+}
